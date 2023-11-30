e333279dc44f3d5f146b67223533da83e54225a7
albint3r <albint3r@gmail.com>
2023-11-30 13:07:04 -0600

    Fix style game card

--- lib/presentation/waiting_rooms/widgets/game_tile.dart
+++ lib/presentation/waiting_rooms/widgets/game_tile.dart
@@ -11,19 +11,38 @@ class GameTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        onTap: game.isFull
-            ? null
-            : () => context.router.replace(
-                  GameRoute(
-                    game: game,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 12,
+        color: Colors.white70,
+        child: ListTile(
+          onTap: game.isFull
+              ? null
+              : () => context.router.replace(
+                    GameRoute(
+                      game: game,
+                    ),
                   ),
-                ),
-        title: Text('Game id: ${game.id}'),
-        leading: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(game.isFull ? 'Is Full' : 'Not Full')],
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Game id: ${game.id.substring(0, 10)}'),
+              const Divider(color: Colors.black),
+              Text('P1 Score: ${game.p1.board.totalScore}'),
+              Text('P2 Score: ${game.p2?.board.totalScore ?? 0}'),
+            ],
+          ),
+          leading: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.do_not_disturb_on_total_silence,
+                color: game.isFull ? Colors.red : Colors.greenAccent,
+              ),
+              Text(game.isFull ? 'Full' : 'Join\nGame'),
+            ],
+          ),
         ),
       ),
     );
