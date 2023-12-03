1fd011569b2f7f2aae68dbe240f9e3ffcf7b1d6e
albint3r <albint3r@gmail.com>
2023-12-03 11:19:46 -0600

    remove imports

--- lib/presentation/game/widgets/podium_area/podium_area.dart
+++ lib/presentation/game/widgets/podium_area/podium_area.dart
@@ -1,10 +1,8 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../application/game/game_bloc.dart';
 import '../../../../domain/waiting_room/player.dart';
-import '../../../core/router/app_router.dart';
 import 'lose_area.dart';
 import 'win_area.dart';
 
