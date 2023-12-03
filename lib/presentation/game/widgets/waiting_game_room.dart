8df951cdc02b7b0b62f83abb88af63af33ace576
albint3r <albint3r@gmail.com>
2023-12-03 10:23:23 -0600

    Add new assets an finish board ui

--- lib/presentation/game/widgets/waiting_game_room.dart
+++ lib/presentation/game/widgets/waiting_game_room.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
+import '../../core/design_system/text/titleh1.dart';
+
 class WaitingGameRoom extends StatelessWidget {
   const WaitingGameRoom({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    final colorScheme = Theme.of(context).colorScheme;
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Waiting Player\nJoin Match', textAlign: TextAlign.center),
-          Gap(20),
-          CircularProgressIndicator(),
+          TitleH1(
+            text: 'Waiting Player\nJoin Match',
+            textAlign: TextAlign.center,
+            color: colorScheme.onBackground,
+          ),
+          const Gap(20),
+          const CircularProgressIndicator(),
         ],
       ),
     );
