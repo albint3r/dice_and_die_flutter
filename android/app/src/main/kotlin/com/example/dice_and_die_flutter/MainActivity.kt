8e961dcb2d10f837b08f613c52a23d326169c8be
albint3r <albint3r@gmail.com>
2023-12-04 22:14:14 -0600

    Add name, icon

--- android/app/src/main/kotlin/com/example/dice_and_die_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dice_and_die_flutter/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.dice_and_die_flutter
+package com.albinter.dice_and_die_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
