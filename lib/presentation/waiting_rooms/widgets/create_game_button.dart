8c90354915ad71f97b5456df00863baf9c027800
albint3r <albint3r@gmail.com>
2023-12-17 13:29:14 -0600

    Fix button responsive

--- lib/presentation/waiting_rooms/widgets/create_game_button.dart
+++ lib/presentation/waiting_rooms/widgets/create_game_button.dart
@@ -3,22 +3,20 @@ import 'package:flutter/material.dart';
 
 import '../../core/design_system/buttons/custom_long_button.dart';
 import '../../core/router/app_router.dart';
+import '../../core/theme/const_values.dart';
 
 class CreateGameButton extends StatelessWidget {
   const CreateGameButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery
-        .of(context)
-        .size;
+    final size = MediaQuery.of(context).size;
     return CustomLongButton(
       text: 'Create Game',
-      width: size.width * .80,
-      onPressed: () =>
-          context.router.replace(
-            GameRoute(),
-          ),
+      width: size.width - (shortButtonWidth + padding + 5),
+      onPressed: () => context.router.replace(
+        GameRoute(),
+      ),
     );
   }
 }
