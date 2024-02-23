5ec94a068cf35cea4aa7e409db1af3e8da1a7df0
albint3r <albint3r@gmail.com>
2024-02-23 09:31:46 -0600

    Move Files to lobby directory

--- lib/presentation/lobby/widgets/body_lobby.dart
+++ lib/presentation/lobby/widgets/body_lobby.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../application/lobby/lobby_bloc.dart';
-import '../../waiting_rooms/widgets/create_game_button.dart';
-import '../../waiting_rooms/widgets/refresh_button.dart';
-import '../../waiting_rooms/widgets/top_indicators.dart';
-import '../../waiting_rooms/widgets/waiting_room_card.dart';
+import 'top_indicators.dart';
+import 'create_game_button.dart';
+import 'refresh_button.dart';
+import 'waiting_room_card.dart';
 
 class BodyLobby extends StatelessWidget {
   const BodyLobby({super.key});
