001520b1fc54d10e0305ec40b2f9538605acfd4f
albint3r <albint3r@gmail.com>
2024-02-24 14:42:06 -0600

    Add token dummy

--- lib/infrastructure/lobby/lobby_data_source_impl.dart
+++ lib/infrastructure/lobby/lobby_data_source_impl.dart
@@ -2,6 +2,7 @@ import 'package:injectable/injectable.dart';
 import 'package:web_socket_channel/src/channel.dart';
 
 import '../../domain/lobby/use_case/i_lobby_data_source.dart';
+import '../core/user_preference.dart';
 
 @Injectable(as: ILobbyDataSource)
 class LobbyDataSourceImpl implements ILobbyDataSource {
@@ -12,7 +13,10 @@ class LobbyDataSourceImpl implements ILobbyDataSource {
   @override
   WebSocketChannel getLobbyChannel() {
     const path = '/v1/lobby/games';
-    final uri = _uri.replace(scheme: 'ws', path: path);
+    final uri = _uri.replace(scheme: 'ws', path: path, queryParameters: {
+      'token':
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDg4OTI2NjYsImlhdCI6MTcwODgwNjI2Niwic3ViIjoiMGY0MjhjYTUtZDExZC0xMWVlLWE0MDYtMDI0MmFjMTMwMDAyIn0.XlcqRNxQvWAeHQpLIrQLxcTauzhvlyu-Brf9WNYar5U"
+    });
     return WebSocketChannel.connect(uri);
   }
 }
