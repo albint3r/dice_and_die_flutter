d452e98b24d9fb115a215816d08373dba4c81c39
albint3r <albint3r@gmail.com>
2024-03-01 08:54:15 -0600

    Big Refactor en listeners and pages screens

--- lib/presentation/game_play/create_or_join_game_page.dart
+++ lib/presentation/game_play/create_or_join_game_page.dart
@@ -7,6 +7,8 @@ import '../../application/sounds_effects/sounds_effects_bloc.dart';
 
 import '../../domain/game_play/entities/game.dart';
 import '../../domain/game_play/entities/player.dart';
+import '../../domain/game_play/enums/enum_game_state.dart';
+import '../../domain/game_play/schemas/response.dart';
 import '../../injectables.dart';
 import 'widgets/body_game_play.dart';
 import 'widgets/bottom_app_game_bar.dart';
@@ -34,24 +36,79 @@ class CreateOrJoinGamePage extends StatelessWidget {
           create: (context) => getIt<SoundsEffectsBloc>(),
         ),
       ],
-      child: Builder(
-        builder: (context) {
-          final state = context.watch<GamePlayBloc>().state;
-          final isOpponentPlayer = state.opponentPlayer is Player;
-          final showBottomAppBar = isOpponentPlayer && !state.existGameError;
-          return SafeArea(
-            child: Scaffold(
-              body: const BodyGamePlay(),
-              bottomNavigationBar:
-                  showBottomAppBar ? const BottomAppGameBar() : null,
-              floatingActionButton: showBottomAppBar
-                  ? const FloatingActionGamePlayButton()
-                  : null,
-              floatingActionButtonLocation:
-                  FloatingActionButtonLocation.centerDocked,
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<GamePlayBloc, GamePlayState>(
+            listenWhen: (pre, curr) {
+              return pre.game?.gameState != curr.game?.gameState &&
+                  curr.game?.gameState == EnumGameState.rollDice;
+            },
+            listener: (context, state) => context.read<SoundsEffectsBloc>().add(
+                  const SoundsEffectsEvent.playRollDice(),
+                ),
+          ),
+          BlocListener<GamePlayBloc, GamePlayState>(
+            listenWhen: (pre, curr) =>
+            pre.game?.winnerPlayer != curr.game?.winnerPlayer &&
+                curr.game?.winnerPlayer is List<Player?>,
+            listener: (context, state) => context.read<GamePlayBloc>().add(
+              const GamePlayEvent.getWinnerPlayer(),
             ),
-          );
-        },
+          ),
+          BlocListener<GamePlayBloc, GamePlayState>(
+            listenWhen: (pre, curr) =>
+            pre.game?.gameState != curr.game?.gameState &&
+                curr.game?.gameState == EnumGameState.selectColumn,
+            listener: (context, state) => context.read<SoundsEffectsBloc>().add(
+              const SoundsEffectsEvent.stopRollDice(),
+            ),
+          ),
+          BlocListener<SoundsEffectsBloc, SoundsEffectsState>(
+            listenWhen: (pre, curr) =>
+            pre.isRollDiceSoundComplete != curr.isRollDiceSoundComplete &&
+                curr.isRollDiceSoundComplete,
+            listener: (context, state) => context.read<GamePlayBloc>().add(
+              const GamePlayEvent.rollDice(),
+            ),
+          ),
+          BlocListener<GamePlayBloc, GamePlayState>(
+            listenWhen: (pre, curr) =>
+            pre.emoteExtrasPlayer != curr.emoteExtrasPlayer &&
+                curr.emoteExtrasPlayer is ResponseEmoteExtras &&
+                !curr.isVisiblePlayerEmote,
+            listener: (context, state) => context.read<GamePlayBloc>().add(
+              const GamePlayEvent.showEmotePlayer(),
+            ),
+          ),
+          BlocListener<GamePlayBloc, GamePlayState>(
+            listenWhen: (pre, curr) =>
+            pre.emoteExtrasOpponent != curr.emoteExtrasOpponent &&
+                curr.emoteExtrasOpponent is ResponseEmoteExtras &&
+                !curr.isVisibleOpponentEmote,
+            listener: (context, state) => context.read<GamePlayBloc>().add(
+              const GamePlayEvent.showEmoteOpponent(),
+            ),
+          ),
+        ],
+        child: Builder(
+          builder: (context) {
+            final state = context.watch<GamePlayBloc>().state;
+            final isOpponentPlayer = state.opponentPlayer is Player;
+            final showBottomAppBar = isOpponentPlayer && !state.existGameError;
+            return SafeArea(
+              child: Scaffold(
+                body: const BodyGamePlay(),
+                bottomNavigationBar:
+                    showBottomAppBar ? const BottomAppGameBar() : null,
+                floatingActionButton: showBottomAppBar
+                    ? const FloatingActionGamePlayButton()
+                    : null,
+                floatingActionButtonLocation:
+                    FloatingActionButtonLocation.centerDocked,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/presentation/game_play/widgets/body_game_play.dart
+++ lib/presentation/game_play/widgets/body_game_play.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../application/game_play/game_play_bloc.dart';
-import '../../../application/sounds_effects/sounds_effects_bloc.dart';
-
-import '../../../domain/game_play/entities/player.dart';
-import '../../../domain/game_play/enums/enum_game_state.dart';
-import '../../../domain/game_play/schemas/response.dart';
 import 'first_person/first_person_area.dart';
 import 'second_person/second_person_area.dart';
 import 'waiting_game_room.dart';
@@ -14,104 +9,29 @@ import 'waiting_game_room.dart';
 class BodyGamePlay extends StatelessWidget {
   const BodyGamePlay({super.key});
 
-  BlocListener<GamePlayBloc, GamePlayState> _rollDiceListener(
-    String text, {
-    bool isButton = true,
-  }) {
-    return BlocListener<GamePlayBloc, GamePlayState>(
-      listenWhen: (pre, curr) {
-        return pre.game?.gameState != curr.game?.gameState &&
-            curr.game?.gameState == EnumGameState.rollDice;
-      },
-      listener: (context, state) => context.read<SoundsEffectsBloc>().add(
-            const SoundsEffectsEvent.playRollDice(),
-          ),
-      child: WaitingGameRoom(
-        text: text,
-        isButton: isButton,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final gamePlay = context.watch<GamePlayBloc>().state;
     final isWaitingOpponent = gamePlay.game?.p2 == null;
     if (gamePlay.existGameError) {
-      return _rollDiceListener(
-        'Connection failed.\nReturning to the lobby...',
+      return const WaitingGameRoom(
+        text: 'Connection failed.\nReturning to the lobby...',
         isButton: false,
       );
     }
     if (gamePlay.isLoading) {
-      return _rollDiceListener('Validating\nWaiting Room');
+      return const WaitingGameRoom(text: 'Validating\nWaiting Room');
     }
     if (isWaitingOpponent) {
-      return _rollDiceListener('Waiting Player\nJoin Match');
+      return const WaitingGameRoom(text: 'Waiting Player\nJoin Match');
     }
-    // todo: REFACTORIZAR LOS LISTENERS EN FUNCIONES QUE SE REPITEN
-    return MultiBlocListener(
-      listeners: [
-        BlocListener<GamePlayBloc, GamePlayState>(
-          listenWhen: (pre, curr) =>
-              pre.game?.winnerPlayer != curr.game?.winnerPlayer &&
-              curr.game?.winnerPlayer is List<Player?>,
-          listener: (context, state) => context.read<GamePlayBloc>().add(
-                const GamePlayEvent.getWinnerPlayer(),
-              ),
-        ),
-        BlocListener<GamePlayBloc, GamePlayState>(
-          listenWhen: (pre, curr) {
-            return pre.game?.gameState != curr.game?.gameState &&
-                curr.game?.gameState == EnumGameState.rollDice;
-          },
-          listener: (context, state) => context.read<SoundsEffectsBloc>().add(
-                const SoundsEffectsEvent.playRollDice(),
-              ),
-        ),
-        BlocListener<GamePlayBloc, GamePlayState>(
-          listenWhen: (pre, curr) =>
-              pre.game?.gameState != curr.game?.gameState &&
-              curr.game?.gameState == EnumGameState.selectColumn,
-          listener: (context, state) => context.read<SoundsEffectsBloc>().add(
-                const SoundsEffectsEvent.stopRollDice(),
-              ),
-        ),
-        BlocListener<SoundsEffectsBloc, SoundsEffectsState>(
-          listenWhen: (pre, curr) =>
-              pre.isRollDiceSoundComplete != curr.isRollDiceSoundComplete &&
-              curr.isRollDiceSoundComplete,
-          listener: (context, state) => context.read<GamePlayBloc>().add(
-                const GamePlayEvent.rollDice(),
-              ),
-        ),
-        BlocListener<GamePlayBloc, GamePlayState>(
-          listenWhen: (pre, curr) =>
-              pre.emoteExtrasPlayer != curr.emoteExtrasPlayer &&
-              curr.emoteExtrasPlayer is ResponseEmoteExtras &&
-              !curr.isVisiblePlayerEmote,
-          listener: (context, state) => context.read<GamePlayBloc>().add(
-                const GamePlayEvent.showEmotePlayer(),
-              ),
-        ),
-        BlocListener<GamePlayBloc, GamePlayState>(
-          listenWhen: (pre, curr) =>
-              pre.emoteExtrasOpponent != curr.emoteExtrasOpponent &&
-              curr.emoteExtrasOpponent is ResponseEmoteExtras &&
-              !curr.isVisibleOpponentEmote,
-          listener: (context, state) => context.read<GamePlayBloc>().add(
-                const GamePlayEvent.showEmoteOpponent(),
-              ),
-        )
+    return ListView(
+      // mainAxisAlignment: MainAxisAlignment.end,
+      children: const [
+        SecondPersonArea(),
+        Divider(color: Colors.black, height: 10, thickness: 5),
+        FirstPersonArea(),
       ],
-      child: ListView(
-        // mainAxisAlignment: MainAxisAlignment.end,
-        children: const [
-          SecondPersonArea(),
-          Divider(color: Colors.black, height: 10, thickness: 5),
-          FirstPersonArea(),
-        ],
-      ),
     );
   }
 }
