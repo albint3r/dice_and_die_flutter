33ae7f9bdd40fbb7b76fbdfa35d3e276d039a2fe
albint3r <albint3r@gmail.com>
2024-03-06 18:43:49 -0600

    Add Version label

--- lib/presentation/auth/login/widgets/body_login.dart
+++ lib/presentation/auth/login/widgets/body_login.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../application/auth/auth_bloc.dart';
 import '../../../../application/login/login_form_bloc.dart';
-import '../../../core/design_system/text/titleh1.dart';
+import '../../../core/design_system/text/text_body_small.dart';
 import '../../../core/theme/const_values.dart';
 import 'login_form.dart';
 import 'name_logo.dart';
@@ -31,6 +31,7 @@ class BodyLogIn extends StatelessWidget {
             child: LoginForm(),
           ),
         ),
+        TextBodySmall('Version 0.0.0.1'),
       ],
     );
   }
