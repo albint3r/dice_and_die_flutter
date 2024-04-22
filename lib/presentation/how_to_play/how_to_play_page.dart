f83abb8d227937e33e361928b69463c7b968a073
albint3r <albint3r@gmail.com>
2024-04-22 13:30:03 -0600

    Add hwo to play

--- lib/presentation/how_to_play/how_to_play_page.dart
+++ lib/presentation/how_to_play/how_to_play_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../core/design_system/app_bar/custom_app_bar.dart';
 import 'widgets/body_how_to_play.dart';
 
 @RoutePage()
@@ -9,9 +11,14 @@ class HowToPlayPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: BodyHowToPlay(),
+        appBar: CustomAppBar(
+          title: 'How To Play',
+          icon: Icons.arrow_back,
+          onPress: () => context.router.pop(),
+        ),
+        body: const BodyHowToPlay(),
       ),
     );
   }
--- lib/presentation/how_to_play/widgets/body_how_to_play.dart
+++ lib/presentation/how_to_play/widgets/body_how_to_play.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+
+import '../../../gen/assets.gen.dart';
+import 'text_bottom.dart';
 
 class BodyHowToPlay extends StatelessWidget {
   const BodyHowToPlay({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final imageHowToPlay = Assets.images.howToPlay;
+    final width = MediaQuery.of(context).size.width;
+    return OnBoardingSlider(
+      finishButtonText: 'Back to Lobby',
+      totalPage: 5,
+      headerBackgroundColor: Colors.yellow,
+      speed: 1,
+      background: [
+        imageHowToPlay.howToPlay1.image(width: width),
+        imageHowToPlay.howToPlay2.image(width: width),
+        imageHowToPlay.howToPlay3.image(width: width),
+        imageHowToPlay.howToPlay4.image(width: width),
+        imageHowToPlay.howToPlay5.image(width: width),
+      ],
+      pageBodies: const [
+        TextBottom(
+          text:
+              'The is played by two player. The purple board is yours and the red one is from your opponent.',
+        ),
+        TextBottom(
+          text:
+              'The board consist in 3 columns each one have there own score counter. On the bottom is the dice and the chat button.',
+        ),
+        TextBottom(
+          text:
+              'Roll the dice and select one of the three columns to put the result.',
+        ),
+        TextBottom(
+          text: 'You score even more points when stacking the same dice in a column.',
+        ),
+        TextBottom(
+          text: 'You remove all opponents dice with the same value when placing a dice in the matching column.',
+        ),
+      ],
+    );
   }
 }
