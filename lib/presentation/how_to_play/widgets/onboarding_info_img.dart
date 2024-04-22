b50d22739b9930ce5f7558150c991ccc508f242c
albint3r <albint3r@gmail.com>
2024-04-22 15:44:14 -0600

    Add elevation

--- lib/presentation/how_to_play/widgets/onboarding_info_img.dart
+++ lib/presentation/how_to_play/widgets/onboarding_info_img.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../gen/assets.gen.dart';
+import '../../core/theme/const_values.dart';
 
 class OnBoardingInfoImg extends StatelessWidget {
   const OnBoardingInfoImg({
@@ -13,6 +14,7 @@ class OnBoardingInfoImg extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return Card(
+      elevation: elevation,
       margin: const EdgeInsets.all(15),
       child: images.image(
         width: width,
